45ed307e247cd583070c2c62ffd351dcc99ef93f
Tamim Azmain <maat1@st-andrews.ac.uk>
2022-10-13 18:16:05 +0100

    wip

---
+++ executor.py
@@ -0,0 +1,26 @@
+import os
+
+from weakref import ref
+from papdl.loader import Loader
+from tensorflow import keras
+import keras
+from keras import layers
+
+
+
+num_classes = 10
+input_shape = (28,28,1)
+
+model = keras.Sequential([
+    keras.Input(shape=input_shape),
+    layers.Conv2D(32, kernel_size=(3,3), activation="relu"),
+    layers.MaxPooling2D(pool_size=(2,2)),
+    layers.Conv2D(64, kernel_size=(3,3), activation="relu"),
+    layers.MaxPooling2D(pool_size=(2,2)),
+    layers.Flatten(),
+    layers.Dropout(0.5),
+    layers.Dense(num_classes, activation="softmax"),
+])
+l = Loader(load_type="object", reference=model)
+model.summary()
+print(l.valid_splits)
