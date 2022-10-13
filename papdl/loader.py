43a5a5efefed220e9a3daf043b3d15cb3aee1fc8
Tamim Azmain <maat1@st-andrews.ac.uk>
2022-10-13 10:09:25 +0100

    Added sample models from Scission

--- papdl/loader.py
+++ papdl/loader.py
@@ -1,5 +1,8 @@
-
+import pickle
+import tensorflow
+import os
 
 class Loader:
     def __init__(self, model_path:str):
-        self.model_path = model_path
+        self.model_path = os.path.abspath(model_path)
+        self.model = pickle.load(model_path)
