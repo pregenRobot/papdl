6703b3403e93f17d1f36fcaf0d6914d31411c89d
Tamim Azmain <maat1@st-andrews.ac.uk>
2023-01-07 23:31:02 +0600

    Added slice config

--- src/papdl/slice/main.py
+++ src/papdl/slice/main.py
@@ -1,9 +1,19 @@
 import click
+from .slice import slice_model
+from keras.models import load_model
 
 @click.command()
-@click.argument("model")
+@click.argument("model_path")
 @click.option("-o", "--output", default=None)
 @click.option("-x", "--optimiser", default="atomic")
 @click.option("-a", "--optimiserargs", default=None)
-def slice(model:str, output:str, optimiser:str, optimiserargs):
+def slice(model_path:str, output:str, optimiser:str, optimiserargs):
     print(f"Slicing model {model} to location {output} optimized with {optimiser} having {optimiserargs}")
+    
+    model = load_model(model_path)
+    print("Loaded Model...")
+    print(model)
+    print("Slicing Model")
+    print(slice_model(model))
+    
+    
